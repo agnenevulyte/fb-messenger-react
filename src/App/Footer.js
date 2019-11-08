ef57a3e011494394da67995a6be28731d9310742
Agne Nevulyte <agne.nevulyte@finsaeurope.com>
2019-11-08 14:23:55 +0000

    installed react router and added <Link/> to footer

--- src/App/Footer.js
+++ src/App/Footer.js
@@ -1,22 +1,25 @@
 import React from "react";
 import LeanJSLogo from "./Layout/LeanJSLogo";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 
 const Footer = () => (
-  <div className="footer">
-    <ul>
-      <li>
-        <a target="_blank" href="https://reactjs.academy">
-          ReactJS Academy
-        </a>
-      </li>
-      <li>by</li>
-      <li>
-        <a target="_blank" href="https://leanjs.com">
-          <LeanJSLogo width={25} />
-        </a>
-      </li>
-    </ul>
-  </div>
+  <Router>
+    <div className="footer">
+      <ul>
+        <li>
+          <Link to={{ pathname: "https://reactjs.academy" }} target="_blank">
+            ReactJS Academy
+          </Link>
+        </li>
+        <li>by</li>
+        <li>
+          <Link to={{ pathname: "https://leanjs.com" }} target="_blank">
+            <LeanJSLogo width={25} />
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </Router>
 );
 
 export default Footer;
